6fb2121813238b0f38ddb4d38278d74f003de3f6
Robert Gallup <bob@robertgallup.com>
2016-12-08 10:34:52 -0800

    Corrected comments in script

--- batch-bmp2hex.sh
+++ batch-bmp2hex.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo 'Creating bitmaps file'
-# Creat the header file
+echo 'Creating file: bitmaps.h'
+# Create the header file
 echo > bitmaps.h
 # Loop through all .bmp files
 for f in *.bmp; do
 # Get the file name
 	stem=${f%.*};
-# Convert file name to upper case for table and file name
+# Convert file name to upper case for table name
 	tbl=$(echo $stem | tr 'a-z' 'A-Z')
 # Echo the table name to the terminal
-	echo "${tbl},"
+	echo "${tbl}"
 # Run the conversion utility
 	python bmp2hex.py $f $tbl >> bitmaps.h
 # Put a couple of blank lines between tables
