5201484f8f6d2b123423f7df23d25a8cf55f124a
Robert Gallup <bob@robertgallup.com>
2016-12-08 10:17:12 -0800

    Added proper argument parser. Added shell script to process all bmp files in local directory

--- bmp2hex.py
+++ bmp2hex.py
@@ -6,20 +6,21 @@
 #
 #	The BMP format is well publicized. The byte order of the actual bitmap is a
 #	little unusual. The image is stored bottom to top, left to right. In addition,
-#	The pixel rows are rounded to DWORDS which are 4 bytes long. To convert this format
-#   to left to right, top to bottom, no byte padding, calculations are necessary
-#	while looping through the rows and bytes of the image. See below for more.
+#	The pixel rows are rounded to DWORDS which are 4 bytes long. SO, to convert this
+#   to left to right, top to bottom, no byte padding. We have to do some calculations
+#	as we loop through the rows and bytes of the image. See below for more
 #
 #	Usage: 
-#	>>>bmp2hex.py <infile outfile>
+#	>>>int8_t2mozzi.py <infile outfile>
 #	
 #	@param infile		The file to convert.
 #	@param tablename	The name of the table to create
-#	@param tablewidth	The number of characters for each row of the output table
-#	@param sizebytes	0, 1, or 2. 0 = auto. 1 = 1-byte for sizes. 2 = 2-byte sizes
+#   @param inverse		"invert", if invert image [optional] 
+#	@param tablewidth	The number of characters for each row of the output table [optional]
+#	@param sizebytes	0, 1, or 2. 0 = auto. 1 = 1-byte for sizes. 2 = 2-byte sizes [optional]
 #	
 #	@author Robert Gallup 2016-02
-#	@fn python bmp2hex.py <infile> <tablename> [tablewidth] [sizebytes]
+#	@fn bmp2hex
 #
 #	Author:    Robert Gallup (bg@robertgallup.com)
 #	License:   MIT Opensource License
@@ -27,43 +28,44 @@
 #	Copyright 2016 Robert Gallup 
 #
 
-import sys, array, os, textwrap, math, random
+import sys, array, os, textwrap, math, random, argparse
 
 def main ():
 
 	# Default parameters
 	infile = ""
 	tablename = ""
-	tablewidth = "16"
-	sizebytes = "0"
+	tablewidth = 16
+	sizebytes = 0
+	invert = False
 
-	# Process input parmeters
-	if len(sys.argv) == 3:
-		[infile, tablename] = sys.argv[1:]
-	elif (len(sys.argv) == 4):
-		[infile, tablename, tablewidth] = sys.argv[1:]
-	elif (len(sys.argv) == 5):
-		[infile, tablename, tablewidth, sizebytes] = sys.argv[1:]
-	else:	
-	     print ('\nUsage: python bmp2hex.py <infile> <tablename> [tablewidth] [sizebytes]\n')
-	     print ('infile       : Input file path')
-	     print ('tablename    : Name of output table')
-	     print ('tablewidth   : Formatted width of output table in hex bytes [default: 16]')
-	     print ('sizebytes    : Byte width of sizes: 0=auto, 1, or 2 (big endian) [default: auto]\n')
-	     sys.exit(1)
+	parser = argparse.ArgumentParser()
+	parser.add_argument ("infile", help="The 1-bit BMP file to convert")
+	parser.add_argument ("tablename", help="The name of the output table")
+	parser.add_argument ("-i", "--invert", help="Inverts bitmap pixels", action="store_true")
+	parser.add_argument ("-w", "--width", help="Output table width in hex bytes [default: 16]", type=int)
+	parser.add_argument ("-b", "--bytes", help="Byte width of BMP sizes: 0=auto, 1, or 2 (big endian) [default: 0]", type=int)
+	args = parser.parse_args()
 
-	# Convert parameters to numbers
-	tablewidth = int(tablewidth)
-	sizebytes = int(sizebytes)
+	infile = args.infile
+	tablename = args.tablename
 
-	bmp2hex(infile, tablename, tablewidth, sizebytes)
+	if args.invert:
+		invert = args.invert
+	if args.width:
+		tablewidth = args.width
+	if args.bytes:
+		sizebytes = args.bytes % 3
 
+	bmp2hex(infile, tablename, tablewidth, sizebytes, invert)
 
 # Return a long int from array (little endian)
 def getLONG(a, n):
 	return (a[n+3] * (2**24)) + (a[n+2] * (2**16)) + (a[n+1] * (2**8)) + (a[n])
 
-def bmp2hex(infile, tablename, tablewidth, sizebytes):
+def bmp2hex(infile, tablename, tablewidth, sizebytes, invert):
+
+	invertbyte = 0x00 if invert else 0xFF
 
 	# Convert tablewidth to characters from hex bytes
 	tablewidth = int(tablewidth) * 6
@@ -116,7 +118,7 @@ def bmp2hex(infile, tablename, tablewidth, sizebytes):
 		for i in range(pixelHeight):
 			for j in range (wordWidth):
 				ndx = BMPOffset + ((pixelHeight-1-i) * paddedWidth) + j
-				outstring += "{0:#04X}".format(values[ndx]) + ", "
+				outstring += "{0:#04X}".format(values[ndx] ^ invertbyte) + ", "
 
 	# Wrap the output buffer. Print. Then, finish.
 	finally:
